43380ce4390172e936cdbcf62cba6ed1fa0b035e
David Soergel <dev@davidsoergel.com>
2010-10-25 18:23:15 +0000

    aeouoeu

--- src/test/java/edu/berkeley/compbio/phyloutils/HugenholtzTaxonomyServiceTest.java
+++ src/test/java/edu/berkeley/compbio/phyloutils/HugenholtzTaxonomyServiceTest.java
@@ -1,5 +1,14 @@
 package edu.berkeley.compbio.phyloutils;
 
+import com.davidsoergel.trees.BasicRootedPhylogeny;
+import com.davidsoergel.trees.NoSuchNodeException;
+import edu.berkeley.compbio.ncbitaxonomy.service.NcbiTaxonomyClient;
+import org.apache.log4j.Logger;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
 /**
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
  * @version $Id$
@@ -18,4 +27,39 @@ public class HugenholtzTaxonomyServiceTest
 		{
 		EnvironmentUtils.setCacheRoot("/tmp/testCache");
 		}*/
+
+	private static final Logger logger = Logger.getLogger(HugenholtzTaxonomyServiceTest.class);
+	HugenholtzTaxonomyService service;
+
+	@BeforeTest
+	public void setUp()
+		{
+		service = new HugenholtzTaxonomyService();
+		//BAD test hack
+		service.setGreengenesRawFilename("/Users/lorax/n/gg.ex.raw");
+		service.setHugenholtzFilename("/Users/lorax/gg.ex.aligned.masked.tree.renum");
+		service.setSynonymService(NcbiTaxonomyClient.getInstance());
+		service.init();
+		}
+
+	@Test
+	public void testRothia() throws NoSuchNodeException
+		{
+		String name = "Rothia mucilaginosa DY-18";
+		int id = service.findTaxidByName(name);
+		logger.info(name + " -> " + id);
+		Set<Integer> idBSet;
+
+		//idBSet = service.findMatchingIds(name);
+
+		//if (idBSet == null || idBSet.isEmpty())
+		//	{
+		idBSet = service.findMatchingIdsRelaxed(name);
+		// this is the only source of NoSuchNodeException that should produce NOTFOUND
+		//	}
+		BasicRootedPhylogeny<Integer> bTree = service.findCompactSubtreeWithIds(idBSet, name);
+		Integer idB = bTree.getShallowestLeaf();
+
+		logger.info(name + " shallowest leaf -> " + idB);
+		}
 	}
