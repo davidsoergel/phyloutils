c412f3e2ceec23a1524666be5072313b1c58d8dd
David Soergel <dev@davidsoergel.com>
2010-10-26 20:26:01 +0000

    aeouoeu

---
+++ src/main/java/edu/berkeley/compbio/phyloutils/NearestNodeFinder.java
@@ -0,0 +1,40 @@
+package edu.berkeley.compbio.phyloutils;
+
+import com.davidsoergel.dsutils.file.IntArrayReader;
+import com.davidsoergel.trees.NoSuchNodeException;
+
+import java.io.IOException;
+
+/**
+ * Load a list of "known" nodes, eg the isolates.  Then for each query sequence (eg from an environment), find the
+ * closest known node, and print the distance (along with whatever other stats are desired, e.g. subtree span).
+ *
+ * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
+ * @version $Id$
+ */
+public class NearestNodeFinder
+	{
+	static HugenholtzTaxonomyService service = new HugenholtzTaxonomyService();
+
+	public static void main(String[] argv) throws IOException, NoSuchNodeException
+		{
+		//service.setGreengenesRawFilename(argv[0]);
+		service.setHugenholtzFilename(argv[0]);
+		//service.setSynonymService(NcbiTaxonomyClient.getInstance());
+		service.init();
+
+		int[] targetIds = IntArrayReader.read(argv[1]);
+		int[] queryIds = IntArrayReader.read(argv[2]);
+
+		for (int queryId : queryIds)
+			{
+			double best = Double.MAX_VALUE;
+			for (int targetId : targetIds)
+				{
+				double d = service.minDistanceBetween(queryId, targetId);
+				best = Math.min(best, d);
+				}
+			System.err.println(queryId + "/t" + best);
+			}
+		}
+	}
